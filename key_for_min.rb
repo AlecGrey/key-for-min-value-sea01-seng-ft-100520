fc163ccba8150b0e311a6d7c67d6c5cef086db5c
Alexander Grey <alecgrey.coding@gmail.com>
2020-08-11 22:14:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,11 @@
 
 def key_for_min_value(name_hash)
   min_key = nil
-  val = 1/0.0
+  min = 1/0.0
   name_hash.each do |key, value|
-    if value < val
+    if value < min
       min_key = key
+      min = value
     end
   end
   min_key
